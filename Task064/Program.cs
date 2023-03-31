0a3954511e369d056b66f305cc030e3ced812c79
Arina Panafidova <panafidovaarina@gmail.com>
2023-03-31 14:54:22 +0300

    Added 2.js

---
+++ Task064/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите натуральное число");
+int number = int.Parse(Console.ReadLine());
+
+NaturalNumber(number);
+
+static void NaturalNumber(int num)
+{
+    if(num <= 0) 
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(num + " ");
+         NaturalNumber(num - 1);
+       
+    }
+}
+
