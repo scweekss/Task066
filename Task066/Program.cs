b12fce638a154590271614a5c8a8d2786fa2f739
Arina Panafidova <panafidovaarina@gmail.com>
2023-03-31 16:30:35 +0300

    Added 3.js

--- Task066/Program.cs
+++ Task066/Program.cs
@@ -6,3 +6,21 @@
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите число N");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int number2 = int.Parse(Console.ReadLine());
+
+static extern int number3(int num1, int num2);
+{
+    if (number1 > number2)
+    {
+       return number1;
+    }
+    else
+    {
+        return 
+        number2 + number3 (number2 + 1, number1);
+    }
+}
+Console.Write($"Сумма {number3(number2, number1)}");
